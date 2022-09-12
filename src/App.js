2db94441615aab7c07ff557d6739291b8df67d1b
Nitin Verma <nitin.vermak@gmail.com>
2022-09-12 16:49:46 +0530

    Added increase & decrease functionality

--- src/App.js
+++ src/App.js
@@ -4,19 +4,29 @@ function App() {
 
   let [counter, setCounter] = useState([0]);
 
-  const  handleCounter = (event) =>{
+  const  increaseCounter = () =>{
     setCounter(prevCount => {
       return [parseInt(counter)+1]
     })
   }
 
-  const intializeCounter = event => {
-     setCounter(event.target.value);
+  const  decreaseCounter = () =>{
+    setCounter(prevCount => {
+      return [parseInt(counter)-1]
+    })
+  }
+  const intializeCounter = (e) => {
+     setCounter(e.target.value);
    };
+
 
   return (
     <div className="App">
-    <button onClick={handleCounter}>Click count:{counter}</button>
+    <button onClick={increaseCounter}>Increase counter</button>
+    <br/>
+    <button onClick={decreaseCounter}>Decrease counter </button>
+
+    <pre> Current Counter: <b>{counter}</b></pre>
     <br/><br/><br/><br/>
     Add intial Value of Click counter:
     <input onChange={intializeCounter} type='text'></input>
